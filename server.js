5b21286780d4dd94d659c5c9fa4ae1d7f39c4d53
MrSpejn <tomek.josiak@gmail.com>
2016-04-06 23:56:11 +0200

    Creating project structure

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.config');
+
+new WebpackDevServer(webpack(config), config.devServer)
+.listen(config.port, 'localhost', (err) => {
+  if (err) {
+    console.log(err);
+  }
+  else {
+    console.log('Listening at localhost:' + config.port);
+  }
+});
