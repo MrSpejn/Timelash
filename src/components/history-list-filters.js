614f67266bc7f8f81c1d18faff759a8f2449d161
Tomke Jósiak <tomek.josiak@gmail.com>
2016-04-14 09:49:42 +0200

    Adding mechanizm to change filters on click

--- src/components/history-list-filters.js
+++ src/components/history-list-filters.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
-export default () => {
+export default (props) => {
   return (
     <ul className="history-list__filters">
-        <li><a className="history-list__filter-item">Last hour</a></li>
-        <li><a className="history-list__filter-item">Today</a></li>
-        <li><a className="history-list__filter-item">Yesterday</a></li>
+        <li>
+          <a className="history-list__filter-item"
+             onClick={() => props.changeFilter('LASTHOUR')}>
+              Last hour
+          </a>
+        </li>
+        <li>
+          <a className="history-list__filter-item"
+             onClick={() => props.changeFilter('TODAY')}>
+              Today
+          </a>
+        </li>
+        <li>
+          <a className="history-list__filter-item"
+             onClick={() => props.changeFilter('YESTERDAY')}>
+              Yesterday
+          </a>
+        </li>
     </ul>
   )
 }
