5443de658e8498d3e8769190a32469fcdc2f33eb
Tomke Jósiak <tomek.josiak@gmail.com>
2016-04-14 09:03:03 +0200

    Moving color palette to scss variables

--- src/components/history-list.js
+++ src/components/history-list.js
@@ -8,7 +8,7 @@ export default class HistoryList extends Component {
         <li key={i}>
           <em className="date">{story.date.format('hh:mmA')}</em>
           {story.name}
-          <span className="hours">
+          <span className="spent-time">
             {`${Math.floor(story.time / 60)}:${leftPad(story.time % 60, 2, '0')}`}
           </span>
         </li>
