2ac9dfa0e3285e4481fc1b76dc81c0522cc993cc
Tomke Jósiak <tomek.josiak@gmail.com>
2016-05-02 12:53:35 +0200

    Fetching profile (with activities) from backend

--- src/actions/types.js
+++ src/actions/types.js
@@ -6,6 +6,9 @@ export const FETCH_UNFINISHED_PROGRESS = 'fetch_unfinished_progress';
 export const FETCH_HISTORY = 'fetch_history';
 export const ADD_TO_HISTORY = 'add_to_history';
 
+export const FETCH_PROFILE = 'FETCH_PROFILE';
+export const UPDATE_PROFILE = 'UPDATE_PROFILE';
+
 export const SIGN_IN = 'SIGN_IN';
 export const SIGN_OUT = 'SIGN_OUT';
 
