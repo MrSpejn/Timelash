85c83e21a92e4da0764b858c29c914917d89f22c
Tomke Jósiak <tomek.josiak@gmail.com>
2016-05-02 12:53:08 +0200

    Fetching history from backend

--- src/actions/history.js
+++ src/actions/history.js
@@ -1,25 +1,35 @@
 import { FETCH_HISTORY, ADD_TO_HISTORY } from './types';
+import axios  from 'axios';
 import moment from 'moment';
 
-export function fetchHistory() {
-  const history = JSON.parse(localStorage.getItem('History')) || [];
+const API_URL = 'http://localhost:3000/history';
 
-  return {
-    type: FETCH_HISTORY,
-    payload: history.map((story) => {
-        story.date = moment(story.date);
-        return story;
-    })
-  };
+export function fetchHistory() {
+  return function (dispatch) {
+      const token = localStorage.getItem('token');
+      axios.get(API_URL, {headers: {authorization: token}})
+      .then(res => {
+        const payload = res.data.map(item => {
+          item.date = moment(item.date);
+          return item;
+        });
+        dispatch({ type: FETCH_HISTORY, payload});
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 }
 
 export function addToHistory(activity) {
-  const currentHistory = JSON.parse(localStorage.getItem('History')) || [];
-  currentHistory.push(activity);
-  localStorage.setItem('History', JSON.stringify(currentHistory));
-
-  return {
-    type: ADD_TO_HISTORY,
-    payload: activity
+  return function (dispatch) {
+    const token = localStorage.getItem('token');
+    axios.post (API_URL, activity, {headers: {authorization: token}})
+    .then(res => {
+      dispatch({ type: ADD_TO_HISTORY, payload: activity});
+    })
+    .catch(err => {
+      console.log(err);
+    });
   }
 }
