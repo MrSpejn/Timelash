b882296de64c8e9cc47a7f1407a20bbf0a1f872b
Tomke Jósiak <tomek.josiak@gmail.com>
2016-05-08 14:56:21 +0200

    Using dates instead of numbers as xAxis domain in line chart

--- src/components/charts/line-chart.js
+++ src/components/charts/line-chart.js
@@ -1,6 +1,6 @@
 import React, {Component}     from 'react';
 import d3                     from 'd3';
-
+import moment                 from 'moment';
 let width  = 920;
 let height = 400;
 let margin = 40;
@@ -22,23 +22,24 @@ class LineChart extends Component {
     svg.append('g').call(xAxis).attr('class', 'x axis').attr('transform', `translate(0, ${height})`);
     svg.append('g').call(yAxis).attr('class', 'y axis').attr('transform', `translate(${margin}, 0)`);
 
-    svg.append('circle').attr('class', 'aura').style({'r': 8, cx: -10, cy: -10});
+    svg.append('circle').attr('class', 'aura').style({'r': 2, cx: -10, cy: -10});
     chartData.forEach((d) => {
       this.drawLine(d, svg, xScale, yScale);
     });
   }
 
-  createScales(data) {
-    const xScale = d3.scale.linear().range([margin, width + margin - 8]);
+  createScales() {
+    const xScale = d3.time.scale().range([margin, width + margin - 8]);
     const yScale = d3.scale.linear().range([height, 20]);
-    xScale.domain([1, d3.max(data, d => d.index)]);
+    xScale.domain([this.props.beginDate, this.props.endDate]);
     yScale.domain([0, 1]);
     return {xScale, yScale};
   }
 
   createAxis(xScale, yScale) {
-    const xAxis = d3.svg.axis().scale(xScale).orient('bottom').ticks(11)
-                    .innerTickSize(-height+20).outerTickSize(0).tickPadding(10);
+    const xAxis = d3.svg.axis().scale(xScale).orient('bottom').ticks(7)
+                    .innerTickSize(-height+20).outerTickSize(0).tickPadding(10)
+                    .tickFormat(date => moment(date).format('DD MMM'));
 
     const yAxis = d3.svg.axis().scale(yScale).orient('left').ticks(12)
                     .innerTickSize(-width+8).outerTickSize(0).tickPadding(10);
@@ -47,9 +48,9 @@ class LineChart extends Component {
   }
 
   drawLine(obj, chart, xScale, yScale) {
-    const line = d3.svg.line().x(d => xScale(d.index)).y(d => yScale(d.value));
+    const line = d3.svg.line().x(d => xScale(d.date)).y(d => yScale(d.value));
     line.interpolate('monotone');
-    const area = d3.svg.area().x(d => xScale(d.index)).y1(d => yScale(d.value)).y0(height);
+    const area = d3.svg.area().x(d => xScale(d.date)).y1(d => yScale(d.value)).y0(height);
     area.interpolate('monotone');
 
     chart.append('path').attr('class', `line line-${obj.color}`).attr('d', line(obj.data));
@@ -58,35 +59,38 @@ class LineChart extends Component {
          .append('circle')
          .attr('class', `circle-${obj.color}`).style({
            'r': 5,
-           cx: (d) => xScale(d.index),
+           cx: (d) => xScale(d.date),
            cy: (d) => yScale(d.value)
          })
          .on('mouseover', function (d) {
            chart.select('.aura').attr('class', `aura line-${obj.color}`)
            .style({
-             cx: xScale(d.index),
+             r: 9,
+             cx: xScale(d.date),
              cy: yScale(d.value)
            });
          })
          .on('mouseout', function () {
-           chart.select('.aura').style({cx: -10, cy: -10});
+           chart.select('.aura').style({r: 2, cx: -10, cy: -10});
          })
   }
 
   generatePlaceholderData() {
+    const beginDate = this.props.beginDate;
     const base = Math.random();
-    const data = d3.range(14).map((v, i) => {
+
+    const data = d3.range(this.props.endDate.diff(beginDate, 'days') + 1).map((v, i) => {
       const value = base - (Math.random() * 0.1);
       return {
         value: value > .5 ? value : value + .5,
-        index: i + 1
+        date: beginDate.clone().add(i, 'days')
       };
     });
-    const data2 = data.map(({value: v, index: i}) => {
+    const data2 = data.map(({value: v, date: d}) => {
       const value = (v - (Math.random() * 0.25)) * 0.9;
       return {
         value: value > 0 ? value : 0,
-        index: i
+        date: d
       };
     });
     return {data, data2};
