c00d66b8fea86ef190980ed89864ebd52f3ade0a
Tomke Jósiak <tomek.josiak@gmail.com>
2016-05-08 14:55:17 +0200

    Adding week timetable to dashboard

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import PieChart from 'components/charts/pie-chart';
 import LineChart from 'components/charts/line-chart';
 import MonthActivityDensity from './month-activity-density';
-
+import CalendarWeekly       from './calendar-weekly';
+import moment from 'moment';
 
 export default class DashboardComponent extends Component {
   render() {
@@ -38,40 +39,47 @@ export default class DashboardComponent extends Component {
         <div className="l-flex">
           <div className='col-lg-8 l-flex dashboard__section'>
             <h2 className="dashboard__section-title">Share of activity in total time</h2>
-            <LineChart id="categories-line-chart" height="250"/>
+            <LineChart beginDate={moment('25 Apr', 'DD MMM')}
+                       endDate={moment('8 May', 'DD MMM')}
+                       id="categories-line-chart" height="250"/>
             <div className="categories-selection">
               <h3 className='dashboard__subsection-title'>Categories</h3>
             </div>
           </div>
+
           <div className='col-lg-4 l-pushbit dashboard__section'>
             <h2 className="dashboard__section-title">Month activity density</h2>
             <MonthActivityDensity />
           </div>
         </div>
 
-        <div className="l-flex dashboard__section categories-pie-charts">
-          <h2 className="dashboard__section-title">Total time splited into categories</h2>
-          <div className='col-sm-3 chart'>
-            <h3 className='dashboard__subsection-title'>Year</h3>
-            <PieChart name="yearly"/>
+        <div className="l-flex categories-pie-charts">
+          <div className='col-md-7 dashboard__section'>
+            <h2 className="dashboard__section-title">Weekly Timetable</h2>
+            <CalendarWeekly />
           </div>
-          <div className='col-sm-3 chart'>
-            <h3 className='dashboard__subsection-title'>Month</h3>
-            <PieChart name="monthly"/>
-          </div>
-          <div className='col-sm-3 chart'>
-            <h3 className='dashboard__subsection-title'>Week</h3>
-            <PieChart name="weekly"/>
-          </div>
-          <div className='col-sm-3 chart'>
-            <h3 className='dashboard__subsection-title'>Day</h3>
-            <PieChart name="dayly"/>
+
+          <div className='col-md-5 l-flex-wrap l-pushbit dashboard__section'>
+            <h2 className="dashboard__section-title">Total time splited into categories</h2>
+            <div className='col-sm-6 chart'>
+              <h3 className='dashboard__subsection-title'>Year</h3>
+              <PieChart name="yearly"/>
+            </div>
+            <div className='col-sm-6 chart'>
+              <h3 className='dashboard__subsection-title'>Month</h3>
+              <PieChart name="monthly"/>
+            </div>
+            <div className='col-sm-6 chart'>
+              <h3 className='dashboard__subsection-title'>Week</h3>
+              <PieChart name="weekly"/>
+            </div>
+            <div className='col-sm-6 chart'>
+              <h3 className='dashboard__subsection-title'>Day</h3>
+              <PieChart name="dayly"/>
+            </div>
           </div>
         </div>
-
-
       </div>
-
     );
   }
 }
